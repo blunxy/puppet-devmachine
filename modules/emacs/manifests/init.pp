489c52cb28671ae653c9c86750e3a7898fe5c7ba
JP Akibako <jp@akibako>
2014-05-10 11:15:34 -0600

    Typo fix.

--- modules/emacs/manifests/init.pp
+++ modules/emacs/manifests/init.pp
@@ -2,7 +2,7 @@ class emacs {
   package { "my-emacs-24.4":
     ensure => installed,
     require => File['/etc/apt/apt.conf.d/99auth'],
-    before => Exec['turn_auth_back_on'],
+#    before => Exec['turn_auth_back_on'],
   }
 
   file { "/etc/apt/apt.conf.d/99auth":
@@ -10,10 +10,9 @@ class emacs {
     group     => root,
     content   => "APT::Get::AllowUnauthenticated yes;",
     mode      => 644,
-    creates   => "/etc/apt/apt.conf.d/99auth",
   }
 
-  exec { "turn_auth_back_on":
-    command => "sudo /bin/rm -f /etc/apt/apt.conf.d/99auth",
-  }
+#  exec { "turn_auth_back_on":
+ #   command => "sudo /bin/rm -f /etc/apt/apt.conf.d/99auth",
+ # }
 }
