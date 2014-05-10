fabed90268a603b63f433d79e28c233d5bbe939f
JP Akibako <jp@akibako>
2014-05-10 11:03:32 -0600

    Try to get unsigned package to install.

--- modules/emacs/manifests/init.pp
+++ modules/emacs/manifests/init.pp
@@ -1,3 +1,18 @@
 class emacs {
-  package { "my-emacs-24.4": ensure => installed }
+  package { "my-emacs-24.4":
+    ensure => installed,
+    require => File['/etc/apt/apt.conf.d/99auth'],
+    before => Exec['turn_auth_back_on'],
+  }
+
+  file { "/etc/apt/apt.conf.d/99auth":
+    owner     => root,
+    group     => root,
+    content   => "APT::Get::AllowUnauthenticated yes;",
+    mode      => 644,
+  }
+
+  exec { "turn_auth_back_on":
+    command => "sudo rm -f '/etc/apt/apt.conf.d/99auth'",
+  }
 }
